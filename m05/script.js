337926dce4bd017d8ab0703b8531955ba11f3cc8
aatiqa234 <aatiqa.imran@bellevuecollege.edu>
2025-02-04 21:16:00 -0800

    script.js

--- m05/script.js
+++ m05/script.js
@@ -1,8 +1,22 @@
-var table =prompt("Enter the any number");
-var msg ='<h2>Multiplication Table for ' +table+ '</h2>';
- for (var i = 1; i <=10; i++){
-   msg +='${table}x${i}' = ${table*i}<br>;
- }
+var table = prompt("Please input a value");            // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = 'h2>Multiplication Table</h2>';              // Message
 
-// Show result 
-document.getElementaryID("output").innerHTML=msg;
+if (operator === 'addition') {
+  // Do addition
+  // while (i < 11) {
+  //  msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+  // i++;
+  }
+else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
