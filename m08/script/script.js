4c1825db9134945428b4e5f041bb0a9faa3dbfdd
aatiqa234 <aatiqa.imran@bellevuecollege.edu>
2025-02-24 17:28:36 -0800

    Update script.js

--- m08/script/script.js
+++ m08/script/script.js
@@ -1,24 +1,27 @@
-// Create a new element and store it in a variable.
 var newEl = document.createElement('li');
 
-// Create a text node and store it in a variable.
 var newText = document.createTextNode('quinoa');
 
-// Attach the new text node to the new element.
 newEl.appendChild(newText);
-function addItem(){
-  var nextItemText = document.getElementById('new-item').value;
-  if (nextItemText){
-    var newEl = document.createElement('li');
-    var newText=document.createTextNode(newItemText);
-    
+var position = document.getElementById('todo');
 
-// Find the position where the new element should be added.
-var position = document.getElementsByTagName('todo')[0];
-
-// Insert the new element into its position.
 position.appendChild(newEl);
-document.getElementById('new-item').value='';
+
+function addItem() {
+  var nextItemText = document.getElementById('new-item').value;
+  
+    if (nextItemText) {
+        var newEl = document.createElement('li');
+    
+        var newText = document.createTextNode(nextItemText);
+
+        newEl.appendChild(newText);
+
+        var position = document.getElementById('todo');
+
+    
+    position.appendChild(newEl);
+
+        document.getElementById('new-item').value = '';
   }
 }
-    
